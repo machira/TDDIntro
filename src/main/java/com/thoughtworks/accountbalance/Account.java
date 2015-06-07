058291838984d285ecc75c6415d4e0b7d21115b8
Machira <raymond.machira@gmail.com>
2015-06-07 16:07:28 -0500

    Account compiles, failing tests

---
+++ src/main/java/com/thoughtworks/accountbalance/Account.java
@@ -0,0 +1,16 @@
+package com.thoughtworks.accountbalance;
+
+/**
+ * Created by machira on 6/7/15.
+ */
+public class Account {
+    int balance;
+
+    Account(int balance){
+        this.balance = balance;
+    }
+
+    public void deposit(int deposit){
+
+    }
+}
--- src/test/java/com/thoughtworks/accountbalance/AccountTests.java
+++ src/test/java/com/thoughtworks/accountbalance/AccountTests.java
@@ -1,12 +1,18 @@
 package com.thoughtworks.accountbalance;
 
+import static org.junit.Assert.assertThat;
+
+import org.junit.Assert;
 import org.junit.Test;
+import static org.hamcrest.core.Is.is;
 
 public class AccountTests {
-//    @Test
-//    public void shouldIncreaseMyBalanceWhenIDepositMoney(){
-//
-//    }
+    @Test
+    public void shouldIncreaseMyBalanceWhenIDepositMoney(){
+        Account account = new Account(100);
+        account.deposit(50);
+        Assert.assertThat(account.balance, is(150));
+    }
 //
 //    @Test
 //    public void shouldDecreaseMyBalanceWhenIWithdrawMoney(){
