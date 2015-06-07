173330ed8ea10c4bf18952e6d94c880e0414db3a
Machira <raymond.machira@gmail.com>
2015-06-07 15:57:40 -0500

    Factorial test4

--- src/main/java/com/thoughtworks/factorial/Factorial.java
+++ src/main/java/com/thoughtworks/factorial/Factorial.java
@@ -2,9 +2,9 @@ package com.thoughtworks.factorial;
 
 public class Factorial {
     public Integer compute(int i) {
-        if(i < 1){
+        if(i <= 1){
             return 1;
         }
-        return i;
+        return i * compute(i -1);
     }
 }
